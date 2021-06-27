7d33c1feff6e27abcbce0f687a002eee19dc360c
Wei Zheng <zangy888@gmail.com>
2021-06-26 21:23:25 -0700

    Commit new guilds

--- src/guilds.js
+++ src/guilds.js
@@ -2,7 +2,10 @@
 const GUILDS = [
   { name: 'Phoenix', roleId: '845903390960779284', column: 'D' },
   { name: 'NWFS', roleId: '845908959642648648', column: 'E' },
-  { name: 'CL', roleId: '847220677550931999', column: 'F' }
+  { name: 'CL', roleId: '847220677550931999', column: 'F' },
+  { name: 'Special', roleId: '858159164542418954', column: 'G' },
+  { name: 'PT', roleId: '858192014499643412', column: 'H' },
+  { name: 'Sir', roleId: '858192979399016458', column: 'I' }
 ]
 
 const ALL_GUILD_ROLE_IDS = GUILDS.map(guild => guild.roleId)
